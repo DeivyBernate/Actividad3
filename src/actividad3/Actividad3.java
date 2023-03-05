6c503de855e66b0722a02315de3d580ad460bc52
DeivyBernate <bernate245@gmail.com>
2023-03-04 22:31:26 -0500

    se agrega el proyecto

---
+++ src/actividad3/Actividad3.java
@@ -0,0 +1,15 @@
+package actividad3;
+public class Actividad3 {
+    public static void main(String[] args) {
+        double N,B,I;                   
+        B=48*5000;
+        I=B*0.125;
+        N=B-I;
+        
+        System.out.println("El Salario bruto es de $" + B);
+        System.out.println("La Retencion en la fuente es de $" + I);
+        System.out.println("El Salario neto es de $" + N);
+    }
+}
+  
+
