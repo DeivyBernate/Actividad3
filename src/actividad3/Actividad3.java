512fe1567ec10d043264131744c5f57eda731393
DeivyBernate <bernate245@gmail.com>
2023-03-04 22:52:02 -0500

    se agrega el proyecto

--- src/actividad3/Actividad3.java
+++ src/actividad3/Actividad3.java
@@ -1,7 +1,8 @@
 package actividad3;
 public class Actividad3 {
     public static void main(String[] args) {
-        double N,B,I;                   
+        double N,B,I; 
+        
         B=48*5000;
         I=B*0.125;
         N=B-I;
